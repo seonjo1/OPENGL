3885225644b9ede47a1c41b9d6b76e9d41a0309f
Seonghyeon Jo <seonjo@c7r1s10.42seoul.kr>
2023-10-02 18:44:41 +0900

    10/2 opengl start

---
+++ cmake_project_example/build/CMakeFiles/cmake_project_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_project_example.dir/src/main.cpp.o"
+  "CMakeFiles/cmake_project_example.dir/src/main.cpp.o.d"
+  "cmake_project_example"
+  "cmake_project_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_project_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
