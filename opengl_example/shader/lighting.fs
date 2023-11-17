837e531813250a367c627849441873fac93854fd
seonjo1 <tjdgus9684@gmail.com>
2023-11-17 17:04:11 +0900

    spotlight

--- opengl_example/shader/lighting.fs
+++ opengl_example/shader/lighting.fs
@@ -9,6 +9,8 @@ uniform vec3 viewPos;
 struct Light
 {
 	vec3 position;
+	vec3 direction; // 방향
+	float cutoff; // 각도
 	vec3 attenuation;
 	vec3 ambient;  // 광원의 ambient strength
 	vec3 diffuse;  // 광원의 분산광
@@ -26,7 +28,7 @@ uniform Material material;
 
 void main()
 {
-	vec3 texColor = texture2D(material.diffuse, texCoord).xyz;
+	vec3 texColor = texture(material.diffuse, texCoord).xyz;
 	vec3 ambient = texColor * light.ambient; // 벡터들의 곱은 각 x, y, z끼리의 곱으로 계산된다.
 
 	float dist = length(light.position - position);
@@ -34,21 +36,27 @@ void main()
 	float attenuation = 1.0 / dot(distPoly, light.attenuation);
 	// attenuation =  1 / (1.0 * Kc + d * Kl + d^2 * Kq) << 감쇠 값
 	vec3 lightDir = normalize(light.position - position) / dist; // - 빛의 방향 벡터
-	vec3 pixelNorm = normalize(normal);
-	// normal을 다시 normalize 하는 이유
-	// - vertex shader에서 계산된 normal은 rasterization되는 과정에서 선형보간이 진행됨
-	// - unit vector간의 선형 보간 결과는 unit vector 보장을 못하기 때문에 normalization 해야함
-	float diff = max(dot(pixelNorm, lightDir), 0.0);
-	vec3 diffuse = diff * texColor * light.diffuse; // max를 이용하여 음수일때는 diffuse = 0
 
-	vec3 specColor = texture2D(material.specular, texCoord).xyz;
-	vec3 viewDir = normalize(viewPos - position);
-	vec3 reflectDir = reflect(-lightDir, pixelNorm); // reflect : 반사광의 방향벡터를 구하는 함수
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess); // specular의 면적 조절
-	// pow를 사용하면 specularShininess가 1보다 크면 반사광의 영역이(원이) 커지고, 1보다 작으면 반사광의 영역이(원이) 작아진다.
-	vec3 specular = spec * specColor * light.specular; // specular의 빛의 정도를 조절
+	float theta = dot(lightDir, normalize(-light.direction));
+	 // 광원과 물체 사이의 벡터와 빛의 방향 벡터 사의의 각도의 코사인값
+	vec3 result = ambient;
 
-	vec3 result = (ambient + diffuse + specular) * attenuation;
+	if (theta > light.cutoff) // 우리가 지정한 cos 값인 light.cutoff 보다 theta가 클경우 각도의 범위를 넘어간 것이므로 빛 x
+	{
+		vec3 pixelNorm = normalize(normal); // 물체표면에 대한 법선벡터
+		float diff = max(dot(pixelNorm, lightDir), 0.0);
+		vec3 diffuse = diff * texColor * light.diffuse;
+
+		vec3 specColor = texture2D(material.specular, texCoord).xyz;
+		vec3 viewDir = normalize(viewPos - position);
+		vec3 reflectDir = reflect(-lightDir, pixelNorm);  // 반사광의 벡터
+		float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+		vec3 specular = spec * specColor * light.specular;
+
+		result += diffuse + specular;
+	}
+
+	result *= attenuation;  // 감쇠
 
 	fragColor = vec4(result, 1.0);
 }
--- opengl_example/src/common.cpp
+++ opengl_example/src/common.cpp
@@ -5,18 +5,17 @@
 // 파일 로딩 함수
 std::optional<std::string> LoadTextFile(const std::string& filename) {
 // c++ 스타일의 파일 읽기
-    std::ifstream fin(filename);
-    if (!fin.is_open()) {
-        SPDLOG_ERROR("failed to open file: {}", filename);
-        return {}; // 아무것도 리턴하지 않음
-    }
-    std::stringstream text;
-    text << fin.rdbuf(); // text에 file 내용을 전부 입력
-    return text.str(); // text에 들어있는 file 내용을 string으로 변환해서 반환
+	std::ifstream fin(filename);
+	if (!fin.is_open()) {
+		SPDLOG_ERROR("failed to open file: {}", filename);
+		return {}; // 아무것도 리턴하지 않음
+	}
+	std::stringstream text;
+	text << fin.rdbuf(); // text에 file 내용을 전부 입력
+	return text.str(); // text에 들어있는 file 내용을 string으로 변환해서 반환
 }
 
-glm::vec3 GetAttenuationCoeff(float distance)
-{
+glm::vec3 GetAttenuationCoeff(float distance) {
 	// Attenuation을 구하는데 사용되는 식인데 교수님이 만든거라 굳이 설명은 안하심
 	// distance를 넣으면 coeffcient값이 3개 튀어나오는 함수라 생각
 	const auto linear_coeff = glm::vec4(8.4523112e-05, 4.4712582e+00, -1.8516388e+00, 3.3955811e+01);
